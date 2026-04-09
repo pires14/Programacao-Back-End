19d261c4728bec41280073d7a2e3f63adc848dda
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-04-09 09:09:18 -0300

    Add files via upload

--- nivelamento/teste.js
+++ nivelamento/teste.js
@@ -33,3 +33,53 @@ if(num2 > num1){
 
 let maiorQue = 10>9;
 console.log(maiorQue);
+
+// 8. Crie duas variáveis com algum valor e exiba a concatenação.
+let nome2 = "Pires";
+let idade = 17;
+console.log(`Meu nome é ${nome2} e tenho ${idade} anos`);
+
+// 9. Descubra o tipo de uma variável
+console.log(typeof nome2); // typeof mostra o tipo da variável
+console.log(typeof idade);
+
+// 10. Converta um numero para string
+let numero = 5;
+console.log(String(numero));
+
+// 11. String para número
+let texto = "123";
+console.log(Number(texto));
+
+// Lações de repetição
+
+// while faz algo enquanto for verdadeiro, se não for verdadeiro ele para.
+
+// 12. Exiba no número de 0 a 10;
+for(let i = 0; i <=10; i++){
+    console.log(i);
+}
+
+// 13. Exiba apenas números pares até 20.
+for(let i = 0; i <=20; i +=2){
+    console.log(i);
+}
+
+// 14. Mostre no console, números de 10 até 
+for(let i = 10; i >0; i--){
+    console.log(i);
+}
+
+// 15. Some numeros de 1 a 5.
+let somaLoop = 0;
+for(let i = 1; i<=5; i++){
+    somaLoop+=i;
+    console.log("valor de i " + i + " valor de somaLoop " + somaLoop);
+}
+console.log(somaLoop);
+
+// 16. Faça a tabuada do 3.
+console.log("Tabuada do 3:")
+for(let i = 1; i <= 10; i++){
+    console.log("3 x " + i + " = " +3*i);
+}
